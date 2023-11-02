223a6662bc7e6343fddb34b079221506569f0408
huzaifa khan <145002182+Huzaifakhan0402@users.noreply.github.com>
2023-11-02 21:20:28 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,97 @@
+print("hello World 1")
+#it is single comment
+""" 
+this is multi line comment
+hccfubi
+hgchgbjn
+"""
+#variable
+number = 24
+print(number)
+
+myNameIs = "huzaifakhan"
+word = 'n'
+
+print(myNameIs)
+print(word)
+
+#datatype
+
+num = 99 #integer
+alphabet = "khan" #string
+num1 = 99.0000 #float
+bool = True #boolean
+type(bool)
+print(type(bool))
+
+#casting-changing data type into another data type
+number = 77
+temp = float(number)
+print(temp)
+
+number = 26.99
+temp = int(number)
+print(temp)
+
+khan = "26"
+temp = float(khan)
+print(temp)
+
+khan1 = 26
+temp = str(khan1)
+print(temp)
+print(type(khan1))
+#
+string2 = "huzaifa"
+string3 = """
+hbjbkj
+jjnkmkn
+bjnkj
+9u989yiuh
+"""
+print(string2)
+print(string3)
+print(string2[4])#index number
+#
+value = "my name is Huzaifa"
+print("my" in value)
+#
+value1 = "helloworld"
+print(value1[5:10])
+
+value2 = "hello friend"
+print(value2[6:])
+
+value3 = "KHAN"
+print(value3.upper())
+#
+value1 = "KHAN"
+value2 = "HUZAIFA"
+
+temp=value1+" "+value2
+print(temp)
+#
+value1 = "HUZAIFA"
+print(value1.startswith("A"))
+
+value2 = "HUZAIFA"
+print(value2.endswith("N"))
+#
+value1 = 1
+value2 = 9
+print(value1>value2)
+#false=0
+#true=1
+
+#
+#conditional statements
+#if else
+value1 = 32
+value2 = 75
+
+if value1 < value2:#less than
+    print("true")
+else:
+    print("false")
+
+
